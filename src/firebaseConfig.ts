dfe9db1ee49248f4e6ee66af575a48b9bb48d01b
Thomas Marques <thomas.marques@edu.devinci.fr>
2024-12-31 10:32:25 +0100

    🚚 Suppression de la dépendance inutile dotenv et des appels associés dans la configuration Firebase

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,10 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import dotenv from "dotenv";
-
-dotenv.config();
-
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
