903024d91afbd12e1cf38ba0b1ca746788a0447c
Thomas Marques <thomas.marques@edu.devinci.fr>
2024-12-31 10:33:10 +0100

    🔧 Mise à jour de l'URL de l'API dans le composant SearchBar et changement de l'intitulé "Acteurs" en "Personnes"

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -19,7 +19,7 @@ export default function SearchBar() {
     useEffect(() => {
         if (query.length > 2 && !isCooldown) {
             setIsCooldown(true);
-            fetch(`/api/search?query=${query}`)
+            fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/search?query=${query}`)
             .then((response) => response.json())
             .then((data) => {
                 if (data && Array.isArray(data.results)) {
@@ -81,11 +81,11 @@ export default function SearchBar() {
             }}></div>
             <div className='fixed top-[25dvh] left-1/2 transform -translate-x-1/2 flex-col gap-2 w-full max-w-xl max-h-96 overflow-auto m-auto rounded-lg p-4 text-base bg-zinc-700 bg-opacity-50 backdrop-blur-md z-50 hidden' id="searchBar">
             <Input type="text" value={query} onChange={(e) => setQuery(e.target.value)} className='w-full border-none text-lg bg-zinc-600 bg-opacity-50 backdrop-blur-sm' placeholder="Rechercher..." />
-            <h4 className="text-lg">Acteurs</h4>
+            <h4 className="text-lg">Personnes</h4>
             <ul className='flex flex-col gap-2'>
                 {results.person.map((person: { id: number; profile_path: string; name: string }) => (
                 <li key={person.id} className="w-full">
-                    <Link href={`/actors/${person.id}`} className="flex items-center gap-2 rounded-sm px-4 hover:bg-zinc-600">
+                    <Link href={`/persons/${person.id}`} className="flex items-center gap-2 rounded-sm px-4 hover:bg-zinc-600">
                         {person.profile_path ? (
                             <Image src={`https://image.tmdb.org/t/p/w500/${person.profile_path}`} alt='Image de profil' width={200} height={200} className='w-8 h-8 object-cover object-top rounded-full' />
                         ) : (
