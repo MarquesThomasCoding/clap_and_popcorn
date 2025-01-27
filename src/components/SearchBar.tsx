4951578006e981ee761db76358d453ea851f716c
Thomas Marques <thomas.marques@edu.devinci.fr>
2025-01-27 16:02:28 +0100

    🐛 Correction de l'affichage de la barre de recherche

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
-import { Search, Share2Icon, UserCircle2Icon } from "lucide-react";
+import { Search, UserCircle2Icon } from "lucide-react";
 import { Movie, Person, Serie } from "@/types/types";
 
 export default function SearchBar(): JSX.Element {
@@ -53,7 +53,7 @@ export default function SearchBar(): JSX.Element {
       };
       fetchResults();
     }
-  }, [query, isCooldown]);
+  }, [query]);
 
   const displayExtendedSearchBar: () => void = () => {
     setShowSearchBar(true);
@@ -61,6 +61,7 @@ export default function SearchBar(): JSX.Element {
   };
 
   const hideExtendedSearchBar: () => void = () => {
+    console.log("hideExtendedSearchBar");
     setShowSearchBar(false);
     document.body.classList.remove("overflow-hidden");
   };
@@ -79,82 +80,80 @@ export default function SearchBar(): JSX.Element {
           className="bg-transparent text-white border-none"
         />
       </div>
-      <div
-        className="fixed inset-0 h-screen bg-black bg-opacity-50 z-40 hidden"
-        id="overlay"
-        onClick={() => {
-          hideExtendedSearchBar();
-        }}
-      ></div>
       {showSearchBar && (
-        <div
-          className="fixed top-[25dvh] left-1/2 transform -translate-x-1/2 flex flex-col gap-2 w-full max-w-xl max-h-96 overflow-auto m-auto rounded-lg p-4 text-base bg-zinc-700 bg-opacity-50 backdrop-blur-md z-50"
-          id="searchBar"
-        >
-          <Input
-            type="text"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            className="w-full border-none text-lg bg-zinc-600 bg-opacity-50 backdrop-blur-sm"
-            placeholder="Rechercher..."
-            autoFocus
-          />
-          <h4 className="text-lg">Personnes</h4>
-          <ul className="flex flex-col gap-2">
-            {results.person.map((person: Partial<Person>) => (
-              <li key={person.id} className="w-full">
-                <Link
-                  href={`/persons/${person.id}`}
-                  className="flex items-center gap-2 rounded-sm px-4 hover:bg-zinc-600"
-                >
-                  {person.profile_path ? (
-                    <Image
-                      src={`https://image.tmdb.org/t/p/w500/${person.profile_path}`}
-                      alt="Image de profil"
-                      width={200}
-                      height={200}
-                      className="w-8 h-8 object-cover object-top rounded-full"
-                    />
-                  ) : (
-                    <UserCircle2Icon className="w-8 h-8 text-white" />
-                  )}
-                  {person.name}
-                  <button className="ml-auto">
-                    <Share2Icon />
-                  </button>
-                </Link>
-              </li>
-            ))}
-          </ul>
-          <Separator className="bg-white" />
-          <h4 className="text-lg">Films</h4>
-          <ul className="flex flex-col gap-2">
-            {results.movie.map((movie: Partial<Movie>) => (
-              <li key={movie.id} className="w-full">
-                <Link
-                  href={`/movies/${movie.id}`}
-                  className="flex items-center gap-2 rounded-sm px-4 hover:bg-zinc-600"
-                >
-                  {movie.title}
-                </Link>
-              </li>
-            ))}
-          </ul>
-          <Separator className="bg-white" />
-          <h4 className="text-lg">Séries</h4>
-          <ul className="flex flex-col gap-2">
-            {results.tv.map((tv: Partial<Serie>) => (
-              <li key={tv.id} className="w-full">
-                <Link
-                  href={`/tv/${tv.id}`}
-                  className="flex items-center gap-2 rounded-sm px-4 hover:bg-zinc-600"
-                >
-                  {tv.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <>
+          <div
+            className="fixed inset-0 h-screen bg-black bg-opacity-50 z-40"
+            id="overlay"
+            onClick={() => hideExtendedSearchBar()}
+          ></div>
+          <div
+            className="fixed top-[25dvh] left-1/2 transform -translate-x-1/2 flex flex-col gap-2 w-full max-w-xl max-h-96 overflow-auto m-auto rounded-lg p-4 text-base bg-zinc-700 bg-opacity-50 backdrop-blur-md z-50"
+            id="searchBar"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <Input
+              type="text"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              className="w-full border-none text-lg bg-zinc-600 bg-opacity-50 backdrop-blur-sm"
+              placeholder="Rechercher..."
+              autoFocus
+            />
+            <h4 className="text-lg">Personnes</h4>
+            <ul className="flex flex-col gap-2">
+              {results.person.map((person: Partial<Person>) => (
+                <li key={person.id} className="w-full">
+                  <Link
+                    href={`/persons/${person.id}`}
+                    className="flex items-center gap-2 rounded-sm px-4 hover:bg-zinc-600"
+                  >
+                    {person.profile_path ? (
+                      <Image
+                        src={`https://image.tmdb.org/t/p/w500/${person.profile_path}`}
+                        alt="Image de profil"
+                        width={200}
+                        height={200}
+                        className="w-8 h-8 object-cover object-top rounded-full"
+                      />
+                    ) : (
+                      <UserCircle2Icon className="w-8 h-8 text-white" />
+                    )}
+                    {person.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <Separator className="bg-white" />
+            <h4 className="text-lg">Films</h4>
+            <ul className="flex flex-col gap-2">
+              {results.movie.map((movie: Partial<Movie>) => (
+                <li key={movie.id} className="w-full">
+                  <Link
+                    href={`/movies/${movie.id}`}
+                    className="flex items-center gap-2 rounded-sm px-4 hover:bg-zinc-600"
+                  >
+                    {movie.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <Separator className="bg-white" />
+            <h4 className="text-lg">Séries</h4>
+            <ul className="flex flex-col gap-2">
+              {results.tv.map((tv: Partial<Serie>) => (
+                <li key={tv.id} className="w-full">
+                  <Link
+                    href={`/tv/${tv.id}`}
+                    className="flex items-center gap-2 rounded-sm px-4 hover:bg-zinc-600"
+                  >
+                    {tv.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
       )}
     </div>
   );
