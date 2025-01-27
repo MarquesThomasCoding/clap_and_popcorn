2738a33e8e88d8e8da972bdcbe66c878af0a60d1
Thomas Marques <thomas.marques@edu.devinci.fr>
2025-01-27 16:01:45 +0100

    🐛 Correction de l'affichage de la description

--- src/components/MediaBanner.tsx
+++ src/components/MediaBanner.tsx
@@ -37,7 +37,7 @@ export default function MediaBanner({
   };
 
   const handleShowOverview = () => {
-    setShowOverview(true);
+    setShowOverview(!showOverview);
   };
 
   const handleAddToSeeMedia = () => {
