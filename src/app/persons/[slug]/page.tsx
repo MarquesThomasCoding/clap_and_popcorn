634b69d6e14ec8deed31461a46e3e791f93210aa
Thomas Marques <thomas.marques@edu.devinci.fr>
2025-01-29 11:53:56 +0100

    📱 Mise à jour du responsive de la page d'acteur

--- src/app/persons/[slug]/page.tsx
+++ src/app/persons/[slug]/page.tsx
@@ -4,7 +4,7 @@ import ActorDetails from "@/components/ActorDetails";
 export default async function PersonPage({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
   return (
-    <main className="flex flex-col gap-8 m-20 p-8">
+    <main className="flex flex-col gap-8 m-5 mt-20 lg:m-20 p-8">
       {/* Load Actor Details Separately */}
       <Suspense fallback={<div>Loading actor details...</div>}>
         <ActorDetails slug={slug} />
--- src/components/ActorBanner.tsx
+++ src/components/ActorBanner.tsx
@@ -13,7 +13,7 @@ export default function ActorBanner(actor: Person): JSX.Element {
 
   return (
     <>
-      <header className="flex items-end gap-8">
+      <header className="flex flex-col lg:flex-row items-end gap-8">
         <Image
           src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
           alt={actor.name}
@@ -39,7 +39,7 @@ export default function ActorBanner(actor: Person): JSX.Element {
           className="fixed inset-0 bg-black bg-opacity-50 z-10 flex justify-center items-center"
           onClick={() => handleShowBiography()}
         >
-          <span className="flex flex-col gap-2 w-1/2 h-1/2 overflow-y-auto rounded-md bg-zinc-900 p-8">
+          <span className="flex flex-col gap-2 lg:w-1/2 h-1/2 overflow-y-auto rounded-md bg-zinc-900 p-8">
             <h4 className="text-xl font-bold border-b border-zinc-400 p-2">
               Biographie
             </h4>
