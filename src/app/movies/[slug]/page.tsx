aa36346f3c5c1622591feabf9132389c72b06a07
Thomas Marques <thomas.marques@edu.devinci.fr>
2025-01-29 10:38:28 +0100

    ✨ Mise à jour des pages de film et série pour utiliser des promesses dans les paramètres

--- src/app/movies/[slug]/page.tsx
+++ src/app/movies/[slug]/page.tsx
@@ -1,13 +1,14 @@
 import MediaDetails from "@/components/MediaDetails";
 import { Suspense } from "react";
 
-export default function Page({ params }: { params: { slug: string } }) {
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
   return (
     <>
       <main className="flex flex-col gap-8">
         {/* Show Loading UI while movie details load */}
         <Suspense fallback={<div>Loading movie details...</div>}>
-          <MediaDetails slug={params.slug} type="movie" />
+          <MediaDetails slug={slug} type="movie" />
         </Suspense>
       </main>
     </>
--- src/app/tv/[slug]/page.tsx
+++ src/app/tv/[slug]/page.tsx
@@ -1,13 +1,14 @@
 import MediaDetails from "@/components/MediaDetails";
 import { Suspense } from "react";
 
-export default function Page({ params }: { params: { slug: string } }) {
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
   return (
     <>
       <main className="flex flex-col gap-8">
         {/* Show Loading UI while movie details load */}
         <Suspense fallback={<div>Loading serie details...</div>}>
-          <MediaDetails slug={params.slug} type="serie" />
+          <MediaDetails slug={slug} type="serie" />
         </Suspense>
       </main>
     </>
